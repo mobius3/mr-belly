8e148489a046a95c0babc6225141648d92b98e08
Leonardo <leonardo_freitas_ext@carrefour.com>
2023-06-16 11:19:04 -0300

    adjust base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mr-belly/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
