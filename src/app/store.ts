b55070c8081534b20b3c769ec96036ce05b9ec2e
Leonardo <leonardo_freitas_ext@carrefour.com>
2023-07-19 22:42:41 -0300

    support for state migrations

--- src/app/store.ts
+++ src/app/store.ts
@@ -6,24 +6,49 @@ import formatISO from 'date-fns/formatISO'
 import { pickFile } from '@/shared/file'
 
 const storageKey = '__mr_belly_state__' as const
+const metaVersion = 1 as const
+const emptyMeta = {
+  state: { invoices: {} },
+  version: metaVersion,
+} as const
+
+const applyStateMigrations = (meta: any): MetaState => {
+  // case: no meta state at all.
+  if (meta === undefined || meta === null || typeof meta !== 'object') {
+    return { ...emptyMeta }
+  }
+
+  // first migration: no version/raw state
+  if (!meta.hasOwnProperty('version')) {
+    meta = {
+      state: meta as RootState,
+      version: 1,
+    }
+  }
+
+  return meta as MetaState
+}
 
 const saveState = (state: RootState) => {
-  const stringified = JSON.stringify(state)
+  const metaState = { state, version: metaVersion }
+  const stringified = JSON.stringify(metaState)
   const compressed = compress(stringified)
   localStorage.setItem(storageKey, compressed)
 }
 
 const exportState = () => {
-  const stringified = JSON.stringify(store.getState())
+  const metaState = { state: store.getState(), version: metaVersion }
+  const stringified = JSON.stringify(metaState)
   const now = formatISO(new Date(), { format: 'basic' })
-  saveAs(new Blob([stringified], { type: 'application/json' }), `mr-belly-${document.location.hostname}-${now}.json`)
+  saveAs(new Blob([stringified], { type: 'application/json' }), `mr-belly-v${metaVersion}-${now}.json`)
 }
 
 const loadState = () => {
-  const compressed = localStorage.getItem(storageKey)
-  if (!compressed) return undefined
+  const compressed = localStorage.getItem(storageKey) || ''
   const stringified = decompress(compressed)
-  return JSON.parse(stringified)
+  const metaState = JSON.parse(stringified)
+  const migratedState = applyStateMigrations(metaState)
+  return migratedState.state
 }
 
 const importState = async () => {
@@ -31,8 +56,8 @@ const importState = async () => {
   if (file.type != 'application/json') return
   const buffer = await file.arrayBuffer()
   const stringified = new TextDecoder().decode(buffer)
-  const state = JSON.parse(stringified)
-  saveState(state)
+  const metaState = JSON.parse(stringified)
+  saveState(metaState)
   window.location.reload()
 }
 
@@ -45,6 +70,7 @@ export const store = configureStore({
 
 store.subscribe(() => saveState(store.getState()))
 
+type MetaState = { version: number; state: any }
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
